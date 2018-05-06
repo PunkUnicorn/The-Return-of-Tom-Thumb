aff551f9877f4455e093a628f4a54f57efc80a24
gnutgnut <jay@flyingspark.eu>
2018-05-06 19:00:10 +0100

    .

--- topdf.ps1
+++ topdf.ps1
@@ -1,6 +1,4 @@
 import-module .\_toolz\posh\PDFTools.psm1
 $path='..\The-Return-of-Tom-Thumb.pdf'
 ls -r|out-ptspdf $path -force
-if(![System.IO.File]::Exists($path)){
-    throw [System.IO.FileNotFoundException] "$path not found.";
-}
+gci -r *.pdf
