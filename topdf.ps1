6de769ffe16ca204cfef9e8050688a6f8ac77eb5
gnutgnut <jay@flyingspark.eu>
2018-05-06 19:42:50 +0100

    -

--- topdf.ps1
+++ topdf.ps1
@@ -1,3 +1,7 @@
 write-host "**topdf.ps1**"
 pandoc --version
-pandoc readme.md -o The-Return-of-Tom-Thumb.epub
+pandoc ^
+  "Prose - Chapter One1.md" ^
+  "Prose - Chapter One2.md" ^
+  "Prose - Chapter Two1.md" ^
+  o The-Return-of-Tom-Thumb.epub
