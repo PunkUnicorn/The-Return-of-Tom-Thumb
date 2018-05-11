73e2a6070f7967f24d773c9a4f948b8b2fb99c53
Matthew <PunkUnicorn100@hotmail.com>
2018-05-11 19:34:47 +0100

    Do lots of stripping... then split!

--- spellchecker.py
+++ spellchecker.py
@@ -12,7 +12,7 @@ import enchant
 d = enchant.Dict("en_UK") # or en_UK, de_DE, fr_FR, en_AU on my system
 print(__doc__)
 for line in sys.stdin:
-    words = line.split().split('').split('"').split('*').split('#').split(';').split(':').split(',').split('?').split('#')
+    words = line.strip('"').strip('*').strip('#').strip(';').strip(':').strip(',').strip('?').strip('#').split() # do lots stripping... then split!
     for word in words:
         if not d.check(word):
             print('YOU SUCK Suggestions for', word, ':', '\n\t'.join(d.suggest(word)))
