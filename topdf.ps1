21d77466cafdfe0b7584825f7356d9a06a8a68d9
Matthew <PunkUnicorn100@hotmail.com>
2018-05-11 20:22:05 +0100

    too much

--- topdf.ps1
+++ topdf.ps1
@@ -1,5 +1,17 @@
 write-host "**topdf.ps1**"
-Get-Content -Path "Prose - Chapter *.md" | python spellchecker.py
+
+Get-ChildItem -Filter *.md | 
+Foreach-Object {
+  $filename = Get-Content $_.FullName
+  Write-Output $filename
+  Get-Content -Path $filename | python spellchecker.py
+}
+
+Write-Output "Chapter One Motherfucker"
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py
+
+
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py
 pandoc --version
 pandoc --css epubstyle.css `
   "title.md" `
