4d01160395d2c6dd498c94985f7dc0530b672838
Matthew <PunkUnicorn100@hotmail.com>
2018-05-11 19:38:21 +0100

    Better stripping

--- spellchecker.py
+++ spellchecker.py
@@ -12,7 +12,16 @@ import enchant
 d = enchant.Dict("en_UK") # or en_UK, de_DE, fr_FR, en_AU on my system
 print(__doc__)
 for line in sys.stdin:
-    words = line.strip('"').strip('*').strip('#').strip(';').strip(':').strip(',').strip('?').strip('#').split() # do lots stripping... then split!
+    # do lots stripping... then split!
+    words = line.strip('"')
+    words = words.strip('*')
+    words = words.strip('#')
+    words = words.strip(';')
+    words = words.strip(':')
+    words = words.strip(',')
+    words = words.strip('?')
+    words = words.strip('#')
+    words = words.split() 
     for word in words:
         if not d.check(word):
             print('YOU SUCK Suggestions for', word, ':', '\n\t'.join(d.suggest(word)))
