64bd9a377310af40a089624e040ab6b83b1e90c5
PunkUnicorn <matthewcocks@hotmail.com>
2018-05-12 10:52:47 +0100

    Tweak

--- topdf.ps1
+++ topdf.ps1
@@ -1,9 +1,7 @@
 write-host "**topdf.ps1**"
 # run tests
 Write-Output "Chapter One Spelling Motherfucker"
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | %{$_.Split(",")} 
-
-#Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename $_[0] -Outcome $_[1] -ErrorMessage $_[2]
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | %{$_.Split(",")} | Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename $_[0] -Outcome $_[1] -ErrorMessage $_[2]
 Write-Output "Chapter One Spelling Ends"
 pandoc --version
 pandoc --css epubstyle.css `
