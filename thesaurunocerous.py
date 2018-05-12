a6104a66e89f75637a84831533fd3bf5b849190f
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 16:44:28 +0100

    tidy and untidy

--- thesaurunocerous.py
+++ thesaurunocerous.py
@@ -5,13 +5,32 @@ import collections
 from nltk.corpus import wordnet
 
 counts = collections.Counter()
+
 for line in sys.stdin:
-    words = line.strip().lower().split() 
+    words = line.replace('\"', '')
+    words = words.replace('*', '')
+    words = words.replace('#', '')
+    words = words.replace(';', '')
+    words = words.replace(':', '')
+    words = words.replace(',', '')
+    words = words.replace('?', '')
+    words = words.replace('.', '')
+    # words = words.replace('\'', '')
+    words = words.replace('\\', '')
+    words = words.replace('/', '')
+    words = words.replace('!', '')
+    words = words.replace(')', '')
+    words = words.replace('(', '')
+    words = words.lower()
+    words = words.split()  
     counts.update(words)
 
 print("total number of words: ", sum(counts.values()))
-for wordOccurance in counts.most_common():
-    hint = "Theasurus here"
-    data = { "Word": wordOccurance, "Status": "Warning", "Hint": hint }
-    print(json.dumps(data))
+
+for word, count in counts.most_common():
+    if (count > 2):
+        hint = "Theasurus here"
+        data = { "Word": word, "Status": "Warning", "Hint": "Occurs " + count + " times" }
+        print(json.dumps(data))
+    else if (count):
 
