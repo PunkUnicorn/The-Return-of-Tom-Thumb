15a70c065899c9dfdccc2e57a35211e9cf946ab7
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 11:22:18 +0100

    second test test

--- topdf.ps1
+++ topdf.ps1
@@ -1,7 +1,7 @@
 write-host "**topdf.ps1**"
 # run tests
 Write-Output "Chapter One Spelling Motherfucker"
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | %{$_.Split("!") |  Select-Object -Property Word, State, Suggestion "-Filename $_.Word" -Outcome "$_.State" -ErrorMessage "$_Suggestion" } #| Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$_.Word" -Outcome "$_.State" -ErrorMessage "$_Suggestion" }
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | %{$_.Split("!") | "-Filename $_[0] -Outcome $_.[1] -ErrorMessage $_[2]" } #| Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$_.Word" -Outcome "$_.State" -ErrorMessage "$_Suggestion" }
 
 Write-Output "Chapter One Spelling Ends"
 pandoc --version
