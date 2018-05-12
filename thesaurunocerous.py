decc87af57a38ebfde88b858c849bc57f353d8f8
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 17:16:46 +0100

    Stats

--- thesaurunocerous.py
+++ thesaurunocerous.py
@@ -6,6 +6,12 @@ from nltk.corpus import wordnet
 
 counts = collections.Counter()
 
+IGNORE_WORDS_THIS_SHORT_OR_LESS = 3 
+
+def statusNone(word, hint):
+    data = { "Word": word,  "Status": "None", "Hint": hint }
+    print(argstrs)
+
 for line in sys.stdin:
     words = line.replace('\"', '')
     words = words.replace('*', '')
@@ -23,15 +29,20 @@ for line in sys.stdin:
     words = words.replace('(', '')
     words = words.lower()
     words = words.split()
-    words = filter(lambda wrd: len(wrd) > 3, words)
+    words = filter(lambda wrd: len(wrd) > IGNORE_WORDS_THIS_SHORT_OR_LESS, words)
+    excludedWords = list(filter(lambda wrd: len(wrd) <= IGNORE_WORDS_THIS_SHORT_OR_LESS, words))
     counts.update(words)
+   
+totalWordCount = sum(counts.values())
+totalWordCount += len(excludedWords)
+statusNone("Count" + str( totalWordCount ))
 
-print("total number of words: ", sum(counts.values()))
+ignoreCounts = collections.Counter(excludedWords)
+statusNone("Occurance of words less than " + str( IGNORE_WORDS_THIS_SHORT_OR_LESS ) + " characters long", ignoreCounts)
+
 
 for word, count in counts.most_common():
-    if (count > 3):
-        hint = "Theasurus here"
-        status = "Ignored" # None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, No
-        data = { "Word": word, "Status": status, "Hint": "Occurs " + str(count) + " times" }
-        print(json.dumps(data))        
-
+    hint = "Theasurus here"
+    status = "Ignored" # None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, No
+    data = { "Word": word, "Status": status, "Hint": "Occurs " + str(count) + " times" }
+    print(json.dumps(data))
