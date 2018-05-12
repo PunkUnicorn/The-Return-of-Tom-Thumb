81a639c479716a42017f46efa58984b242f528a2
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 21:00:32 +0100

    Tidy fix

--- topdf.ps1
+++ topdf.ps1
@@ -1,11 +1,11 @@
 write-host "**topdf.ps1**"
 # run tests
 Write-Output "Chapter One Spelling Motherfucker"
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | fl  # | %{ ,$_.Split("!") | fl $_ } #Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename -ErrorMessage
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | fl  #Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename -ErrorMessage
 Write-Output "Chapter One Spelling Ends"
-Write-Output "Chapter One thesaurunocerous Boom (sorry takes a while to download)"
+Write-Output "Chapter One Thesaurunocerous Boom (sorry takes a while to download)"
 Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | fl 
-Write-Output "Chapter One Mthesaurunocerous Ends"
+Write-Output "Chapter One Thesaurunocerous Ends"
 pandoc --version
 pandoc --css epubstyle.css `
   "title.md" `
