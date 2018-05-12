d7e51b296df5e5fcca9c50a217f6172f36eeb850
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 16:18:43 +0100

    pulled back

--- thesaurunocerous.py
+++ thesaurunocerous.py
@@ -2,17 +2,14 @@ from __future__ import print_function
 import sys
 import json
 import collections
-from py_thesaurus import Thesaurus
+from nltk.corpus import wordnet
 
 counts = collections.Counter()
 
 for line in sys.stdin:
     words = line.strip().lower().split() 
     counts.update(words)
-
-for needSuggestions in counts.most_common(7):
-    new_instance = Thesaurus(needSuggestions)
-    data = { "Word": needSuggestions, "Status":"Warning", "Hint": new_instance.get_synonym() }
-    print(json.dumps(data))
+    
+print(counts.most_common(7))
 
 
