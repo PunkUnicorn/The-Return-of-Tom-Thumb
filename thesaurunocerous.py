3fff89c3f4913abd051a441dc1cd45f08dc6e0e2
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 17:00:40 +0100

    chop words

--- thesaurunocerous.py
+++ thesaurunocerous.py
@@ -22,7 +22,8 @@ for line in sys.stdin:
     words = words.replace(')', '')
     words = words.replace('(', '')
     words = words.lower()
-    words = words.split()  
+    words = words.split()
+    words = filter(wrd: len(wrd) > 3, words)
     counts.update(words)
 
 print("total number of words: ", sum(counts.values()))
