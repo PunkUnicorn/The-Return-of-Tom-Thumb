be15f7a40a2113b987fdb673740977ac1c5f9837
Matthew <PunkUnicorn100@hotmail.com>
2018-05-12 17:37:09 +0100

    fix

--- thesaurunocerous.py
+++ thesaurunocerous.py
@@ -9,8 +9,8 @@ ignoredCounts = collections.Counter()
 
 IGNORE_WORDS_THIS_SHORT_OR_LESS = 3 
 
-def statusNone(title, hint):
-    data = { "Word": title,  "Status": "None", "Hint": hint }
+def statusMessage(title, hint):
+    data = { "Word": title,  "Status": "Message", "Hint": hint }
     print(json.dumps(data))
 
 for line in sys.stdin:
@@ -37,14 +37,15 @@ for line in sys.stdin:
    
 totalWordCount = sum(counts.values())
 totalWordCount += sum(ignoredCounts.values())
-statusNone("Total number of words", str( totalWordCount ))
+statusMessage("Total number of words", str( totalWordCount ))
 
 ignoreCounts = collections.Counter(ignoredWordsList)
 uniqueIgnoredWords = list(set(ignoredWordsList))
-statusNone("Occurance of words less than " + str( IGNORE_WORDS_THIS_SHORT_OR_LESS ) + " characters long", ", ".join(uniqueIgnoredWords))
+ignoreHint = ", ".join(ignoreCounts)
+statusMessage("Occurance of words less than " + str( IGNORE_WORDS_THIS_SHORT_OR_LESS ) + " characters long", ignoreHint)
 
 for word, count in counts.most_common():
     hint = "Theasurus here"
-    status = "Skipped" # None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, No
-    data = { "Word": word, "Status": status, "Hint": "Occurs " + str(count) + " times" }
+    status = "Warning" # None, Running, Passed, Failed, Ignored, Skipped, Inconclusive, No
+    data = { "Word": word, "Status": status, "Hint": "Occurs " + str(count) + " times: " + hint }
     print(json.dumps(data))
