b92288198a9c0e1457130cc66c42a90e800007b8
PunkUnicorn <matthewcocks@hotmail.com>
2018-05-13 13:21:33 +0100

    Hack test

--- topdf.ps1
+++ topdf.ps1
@@ -7,11 +7,11 @@ Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "suddently" -ErrorM
 Write-Output "Chapter One Spelling Motherfucker"
 Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | fl
 
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | fl
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } | fl
 
 Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ fl }
 
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$($_.Word)" -ErrorMessage "$($_.Hint)" -Outcome "$($_.Status)" }
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json |  %{ fl } | %{ Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$($_.Word)" -ErrorMessage "$($_.Hint)" -Outcome "$($_.Status)" }
 
 Write-Output "Chapter One Spelling Ends"
 Write-Output "Chapter One Thesaurunocerous Boom (sorry takes a while to download)"
