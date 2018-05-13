37d980a07ff2d496cd67db5f0004cf41d4790ffb
Matthew <PunkUnicorn100@hotmail.com>
2018-05-13 16:59:03 +0100

    tweak

--- topdf.ps1
+++ topdf.ps1
@@ -7,12 +7,12 @@ write-host "**topdf.ps1**"
 Write-Output "Chapter One Spelling Boom" #Add as test fails, and to the message window as errors
 Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } | fl
 Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } | %{ Add-AppveyorMessage -Message "$($_.Word) - Chapter One - Spelling" -Details "$($_.Hint)" -Category "Error" }
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } | %{ Add-AppveyorTest -Name "$($_.Word) - Spelling" -Framework NUnit -Filename "$($_.Word) - Chapter One - Spelling" -ErrorMessage "$($_.Hint)" -Outcome "$($_.Status)" }
+Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } | %{ Add-AppveyorTest -Name "$($_.Word) - Spelling" -Framework NUnit -Filename "$($_.Hint)" -ErrorMessage "$($_.Word)? $($_.Hint)" -Outcome "$($_.Status)" }
 Write-Output "Chapter One Spelling Ends"
 Write-Output "Chapter One Thesaurunocerous Boom" #Only add to the messages window
 Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results } | fl
 Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results } | %{ Add-AppveyorMessage -Message "$($_.Word)$(if ( $($_.Occurs) -gt '0' ){$(" x ");$($_.Occurs)} ) - Chapter One - Thesaurunocerous" -Details "$($_.Hint)" -Category "$($_.Status)" }
-Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results } | %{ fl | Out-File -FilePath "Chapter-One-Words.txt" }
+Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results } | %{ fl | Out-File -FilePath "Chapter-One-Words.txt" -Append }
 Write-Output "Chapter One Thesaurunocerous Ends"
 pandoc --version
 pandoc --css epubstyle.css `
