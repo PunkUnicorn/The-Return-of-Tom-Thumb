b2f4df202b7b1d9117f29ad829b8c665c7647a0e
Matthew <PunkUnicorn100@hotmail.com>
2018-05-13 09:52:53 +0100

    revert

--- topdf.ps1
+++ topdf.ps1
@@ -1,16 +1,16 @@
 write-host "**topdf.ps1**"
 # run tests
-#Add-AppveyorMessage -Message "Matthew is cool" -Details "Hint" -Category "Error" #Information | Warning | Error
-#Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "Word" -ErrorMessage "Hint" -Outcome "Failed" #Passed, Failed, Ignored, Skipped, Inconclusive, NotFound, Cancelled, NotRunnable
+
+Add-AppveyorMessage -Message "Yo dawg" -Details "Boomshanka" -Category "Information" #Information | Warning | Error
+Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "suddently" -ErrorMessage "suggest: two or too or to or 2" -Outcome "Failed" #Passed, Failed, Ignored, Skipped, Inconclusive, NotFound, Cancelled, NotRunnable
+
 Write-Output "Chapter One Spelling Motherfucker"
-
 Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | fl
-Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$_.Word" -ErrorMessage "$_.Hint" -Outcome "$_.Status"
-
+#Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename "$_.Word" -ErrorMessage "$_.Hint" -Outcome "$_.Status"
 Write-Output "Chapter One Spelling Ends"
-#Write-Output "Chapter One Thesaurunocerous Boom (sorry takes a while to download)"
-#Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | fl 
-#Write-Output "Chapter One Thesaurunocerous Ends"
+Write-Output "Chapter One Thesaurunocerous Boom (sorry takes a while to download)"
+Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | fl 
+Write-Output "Chapter One Thesaurunocerous Ends"
 pandoc --version
 pandoc --css epubstyle.css `
   "title.md" `
