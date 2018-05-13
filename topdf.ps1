4a121c2f63c1af48dd4e4309b4cd1f5ca78fb3f0
Matthew <PunkUnicorn100@hotmail.com>
2018-05-13 18:28:30 +0100

    simplify

--- topdf.ps1
+++ topdf.ps1
@@ -4,14 +4,14 @@ Write-Output "Chapter One Spelling Boom" #Add as test fails, and to the message 
 $chapterOneSpelling = Get-Content -Path "Prose - Chapter One*.md" | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } 
 $chapterOneSpelling | fl
 $chapterOneSpelling | fl | Out-File -FilePath "Chapter-One-Spelling.txt" -Append
-$chapterOneSpelling | %{ Add-AppveyorMessage -Message "$($_.Word) - Chapter One - Spelling" -Details "$($_.Hint)" -Category "Error" }
+$chapterOneSpelling | %{ Add-AppveyorMessage -Message "$($_.Word) - Chapter One" -Details "$($_.Hint)" -Category "Error" }
 $chapterOneSpelling | %{ Add-AppveyorTest -Name "$($_.Word) - Spelling" -Framework NUnit -Filename "$($_.Hint)" -ErrorMessage "$($_.Word)? $($_.Hint)" -Outcome "$($_.Status)" }
 Write-Output "Chapter One Spelling Ends"
 Write-Output "Chapter One Thesaurunocerous Boom" #Only add to the messages window
 $chapterOneTheasurus = Get-Content -Path "Prose - Chapter One*.md" | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results }
 $chapterOneTheasurus | fl
 $chapterOneTheasurus | fl | Out-File -FilePath "Chapter-One-Words.txt" -Append
-$chapterOneTheasurus | %{ Add-AppveyorMessage -Message "$($_.Word)$(if ( $($_.Occurs) -gt '0' ){$(" x ");$($_.Occurs)} ) - Chapter One - Thesaurunocerous" -Details "$($_.Hint)" -Category "$($_.Status)" }
+$chapterOneTheasurus | %{ Add-AppveyorMessage -Message "$($_.Word) x $($_.Occurs) - Chapter One" -Details "$($_.Hint)" -Category "$($_.Status)" }
 Write-Output "Chapter One Thesaurunocerous Ends"
 pandoc --version
 pandoc --css epubstyle.css `
