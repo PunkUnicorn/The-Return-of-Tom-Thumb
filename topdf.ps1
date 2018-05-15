1e63b9bfe4d1a153a913090f4d16a389901a5f1e
Matthew <PunkUnicorn100@hotmail.com>
2018-05-15 23:58:38 +0100

    More untidy

--- topdf.ps1
+++ topdf.ps1
@@ -1,11 +1,9 @@
 write-host "**topdf.ps1**"
-# run tests
-Write-Output "Spelling Boom" #Add as test fails, and to the message window as errors
-Write-Output "Spelling Exceptions start:"
-Get-Content -Path "spellchecker.exceptions.txt" | Write-Output
-Write-Output "Spelling Exceptions end!"
 
-# Fancy quote solution: https://stackoverflow.com/questions/6968270/replacing-smart-quotes-in-powershell
+#
+# Fancy quote solution: 
+# https://stackoverflow.com/questions/6968270/replacing-smart-quotes-in-powershell
+#
 Function Replace-FancyQuotes {
 	Process {
 		$fancySingleQuotes = "[\u2019\u2018]" 
@@ -18,52 +16,59 @@ Function Replace-FancyQuotes {
 	}
 }
 
-# Fancy quote solution: https://stackoverflow.com/questions/6968270/replacing-smart-quotes-in-powershell
-#$fancySingleQuotes = "[\u2019\u2018]" #Strip out fancy single and double quotes for spellchecking etc, or python goes ballistic
-#$fancyDoubleQuotes = "[\u201C\u201D]" 
-$chapterName = "Chapter One"
-$spellingFailFilename = "Chapter-One-Spelling.txt"
+#
+# Spellchecks chapter files by filename convension
+# Outputs test result messages and all the jazz
+#
+Function Spellcheck-Chapter($chapterName, $spellingFailFilename) {
+	Write-Output ".oO($chapterName Spelling starts:)"
+	
+	$chapter = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8
+	$chapterSpelling = $chapterOne | Replace-FancyQuotes | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } 
+	$chapterSpelling | fl; $chapterOneSpelling | fl | Out-File -FilePath $spellingFailFilename -Append
+	$chapterSpelling | %{ Add-AppveyorMessage -Message "$($_.Word) - $chapterName" -Details "$($_.Hint)" -Category "Error" }
+	$chapterSpelling | %{ Add-AppveyorTest -Name "$($_.Word) - Spelling" -Framework NUnit -Filename "$($_.Hint)" -ErrorMessage "$($_.Word)? $($_.Hint)" -Outcome "$($_.Status)" }
 
-Write-Output "$chapterName Spelling..."
-$chapterOne = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8
-#$chapterOneSpelling = Get-Content -Path "Prose - Chapter One*.md" -Encoding UTF8 | `
-#%{ `
-#	$_ = `
-#	[regex]::Replace($_, $fancySingleQuotes, "'")
-#	[regex]::Replace($_, $fancyDoubleQuotes, '"') `
-#} | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } 
-$chapterOneSpelling = $chapterOne | Replace-FancyQuotes | python spellchecker.py | ConvertFrom-Json | %{ $_.Results } 
-$chapterOneSpelling | fl; $chapterOneSpelling | fl | Out-File -FilePath $spellingFailFilename -Append
-$chapterOneSpelling | %{ Add-AppveyorMessage -Message "$($_.Word) - $chapterName" -Details "$($_.Hint)" -Category "Error" }
-$chapterOneSpelling | %{ Add-AppveyorTest -Name "$($_.Word) - Spelling" -Framework NUnit -Filename "$($_.Hint)" -ErrorMessage "$($_.Word)? $($_.Hint)" -Outcome "$($_.Status)" }
-
-$spellingResults = $null;
-$spellingResults = Get-Content -Path $spellingFailFilename 
-If ($spellingResults -eq $null)
-{
-	Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename $chapterName -ErrorMessage "All passed" -Outcome "Passed"
-	Write-Output "No spelling errors"
+	$spellingResults = $null;
+	$spellingResults = Get-Content -Path $spellingFailFilename 
+	If ($spellingResults -eq $null)
+	{
+		Add-AppveyorTest -Name "Spelling" -Framework NUnit -Filename $chapterName -ErrorMessage "All passed" -Outcome "Passed"
+		Write-Output "No spelling errors"
+	}
+	
+	Write-Output ".oO($chapterName Spelling ends!)"
 }
-Write-Output "$chapterName Spelling Ends!"
 
 
-Write-Output "Spelling Ends"
 
-Write-Output "Chapter One Thesaurunocerous Boom" #Only add to the messages window
-$chapterOneTheasurus = Get-Content -Path "Prose - Chapter One*.md" | `
-%{ `
-	$_ = `
-	[regex]::Replace($_, $fancySingleQuotes, "'")
-	[regex]::Replace($_, $fancyDoubleQuotes, '"') `
-} | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results }
+# run tests
+
+Write-Output "Spelling Starts   ╦̵̵̿╤─ ҉ ~ •" 
+Write-Output ".oO(Spelling Exceptions start:)"
+Get-Content -Path "spellchecker.exceptions.txt" | Write-Output
+Write-Output ".oO(Spelling Exceptions end!)"
+Spellcheck-Chapter("Chapter One", "Chapter-One-Spelling.txt")
+Spellcheck-Chapter("Chapter Two", "Chapter-One-Spelling.txt")
+Write-Output "Spelling Ends   ╦̵̵̿╤─ ҉ ~ •"
+
+$chapterName = "Chapter One"
+$wordsFilename = "Chapter-One-Words.txt"
+Write-Output "Thesaurunocerous Starts \̅_̅/̷̚ʾ     \̅_̅/̷̚ʾ"
+Write-Output ".oO($chapterName Thesaurunocerous starts:)"
+$chapter = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes 
+$chapterTheasurus = $chapter | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results }
 $chapterOneTheasurus | fl
-$chapterOneTheasurus | fl | Out-File -FilePath "Chapter-One-Words.txt" -Append
-$chapterOneTheasurus | %{ Add-AppveyorMessage -Message "$($_.Word) x $($_.Occurs) - Chapter One" -Details "$($_.Hint)" -Category "$($_.Status)" }
-Write-Output "Chapter One Thesaurunocerous Ends"
+$chapterOneTheasurus | fl | Out-File -FilePath $wordsFilename -Append
+$chapterOneTheasurus | %{ Add-AppveyorMessage -Message "$($_.Word) x $($_.Occurs) - $chapterName" -Details "$($_.Hint)" -Category "$($_.Status)" }
+Write-Output ".oO($chapterName Thesaurunocerous end!)"
+Write-Output "Thesaurunocerous Ends \̅_̅/̷̚ʾ     \̅_̅/̷̚ʾ"
+
 pandoc --version
 pandoc --css epubstyle.css `
   "title.md" `
   "Prose - Chapter One1.md" `
   "Prose - Chapter One2.md" `
   "Prose - Chapter One3.md" `
+  "Prose - Chapter Two1.md" `
   -o The-Return-of-Tom-Thumb.epub
