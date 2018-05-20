cad6971d998a6c2eb23c8a7f667a51ece7aafaef
Matthew <PunkUnicorn100@hotmail.com>
2018-05-20 10:28:21 +0100

    Word w/o header into thesaurus

--- topdf.ps1
+++ topdf.ps1
@@ -130,16 +130,10 @@ Write-Output $chapterContent | Measure-Object Count -Sum -Maximum | Select Count
 	@{label="Word count";Expression={{$_.Sum}},
 	@{label="Maximum occurrence";Expression={{$_.Maximum}} | fl
 
-# Thesaurus look up 
-$chapterContent | `
-	Where { $_.Count -gt 2 } | `
-	Where { $_.Length -gt 2 } | `
-	Select Word 
-
 Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes | python wordcounter.py | ConvertFrom-Csv `
 	Where { $_.Count -gt 2 } | `
 	Where { $_.Length -gt 2 } | `
-	Select Word | `
+	foreach { $_.Word } | `
 	python theasaurus.py | `
 	ConvertFrom-Csv 
 
