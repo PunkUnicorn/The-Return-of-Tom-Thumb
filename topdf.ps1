57895bdca93f17ecc59965f20f5d4b050eda2531
Matthew <PunkUnicorn100@hotmail.com>
2018-05-20 22:41:56 +0100

    txt

--- topdf.ps1
+++ topdf.ps1
@@ -153,9 +153,16 @@ pandoc --css epubstyle.css `
   "The-Return-of-Tom-Thumb.md" `
   -o The-Return-of-Tom-Thumb.epub
 
-# Make the audio book
+#html version (for cross converting)
 pandoc --css epubstyle.css `
   "title.md" `
   "The-Return-of-Tom-Thumb.md" `
+  -o The-Return-of-Tom-Thumb.html
+	
+# Make the audio book
+pandoc --css epubstyle.css `
+  "title.md" `
+  "The-Return-of-Tom-Thumb.html" `
   -o The-Return-of-Tom-Thumb.txt
+	
 #Get-Content The-Return-of-Tom-Thumb.txt -Encoding UTF8 | Replace-FancyQuotes | python googleTextToSpeech.py
