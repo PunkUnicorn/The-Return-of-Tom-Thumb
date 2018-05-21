65cc769cc20002c4ad75ad3e1eb4c7b5ca60ce1b
Matthew <PunkUnicorn100@hotmail.com>
2018-05-22 00:21:24 +0100

    debug text to speech  glitches

--- googleTextToSpeech.py
+++ googleTextToSpeech.py
@@ -4,16 +4,24 @@ import io
 
 # START
 saveFilename = 'audio.mp3'
-if len(sys.argv) == 3:
+debugFilename = ''
+if len(sys.argv) > 2: # -o <output filename.mp3>
     saveFilename = str(sys.argv[2])
-
+if len(sys.argv) > 3: # -d <debug filename.txt>
+    debugFilename = str(sys.argv[4])    
 with io.BytesIO() as f:
     for line in sys.stdin:
         if (line is None):
             continue;
         if (len(line) == 0):
             continue;
+            
         word = ' '.join(line.split())
+        
+        if len(debugFilename) > 0:
+            with open(debugFilename, 'a') as debugFile
+                debugFile.write(word)
+                
         tts = gTTS(text=word + '\n', lang='en-GB')
         try: 
             tts.write_to_fp(f)
