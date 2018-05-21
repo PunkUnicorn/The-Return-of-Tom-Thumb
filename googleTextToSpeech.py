23fe4579a234f16e77a4658e9100ab99b8f023e5
Matthew <PunkUnicorn100@hotmail.com>
2018-05-22 00:44:12 +0100

    space

--- googleTextToSpeech.py
+++ googleTextToSpeech.py
@@ -3,21 +3,27 @@ from gtts import gTTS
 import io
 
 # START
+first=True
 saveFilename = 'audio.mp3'
 debugFilename = ''
 if len(sys.argv) > 2: # -o <output filename.mp3>
     saveFilename = str(sys.argv[2])
 if len(sys.argv) > 3: # -d <debug filename.txt>
     debugFilename = str(sys.argv[4])    
+    
 with io.BytesIO() as f:
     for line in sys.stdin:
         if (line is None):
             continue;
+            
         if (len(line) == 0):
             continue;
             
         word = ' '.join(line.split())
-        
+        if (first):
+            word = "  " + word
+            first=False
+            
         if len(debugFilename) > 0:
             with open(debugFilename, 'a') as debugFile:
                 debugFile.write(word)
@@ -26,8 +32,9 @@ with io.BytesIO() as f:
         try: 
             tts.write_to_fp(f)
         except:
-            continue;
-
+            continue;            
+    #end for line in sys.stdin
+    
     f.flush()
     f.seek(0)
     stuff = bytes(f.read())
