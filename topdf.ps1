99d7997094fd9c9dbc55fc3fea028cd4866070a4
Matthew <PunkUnicorn100@hotmail.com>
2018-05-24 23:17:20 +0100

    works?

--- topdf.ps1
+++ topdf.ps1
@@ -19,17 +19,18 @@ Function Replace-FancyQuotes {
 }
 
 #
-# Destroy quote solution: 
-# https://stackoverflow.com/questions/6968270/replacing-smart-quotes-in-powershell
+# Destroy quotes:
 #
-Function Destroy-FancyQuotes {
+Function Destroy-Quotes {
 	Process {
 		$fancySingleQuotes = "[\u2019\u2018]" 
 		$fancyDoubleQuotes = "[\u201C\u201D]" 		
 		%{ `
 			$_ = `
 			[regex]::Replace($_, $fancySingleQuotes, " ")
-			[regex]::Replace($_, $fancyDoubleQuotes, ' ') `
+			[regex]::Replace($_, $fancyDoubleQuotes, ' ') 
+			[regex]::Replace($_, "'", " ")
+			[regex]::Replace($_, '"', ' ') `
 		}
 	}
 }
@@ -73,8 +74,7 @@ Function Spellcheck-Chapter($chapterName, $spellingFailFilename) {
 			-ErrorMessage "All passed" `
 			-Outcome "Passed"
 			
-		Write-Output "No spelling errors"
-		
+		Write-Output "No spelling errors"		
 	}
 	
 	Write-Output "$chapterName Spelling ends!"
@@ -95,8 +95,7 @@ Function Spellcheck-DumpExceptions() {
 # Outputs word stat results messages etc
 #
 Function Thesaurunocerous-Chapter($chapterName, $wordsFilename) {
-	Write-Output "$chapterName Thesaurunocerous starts:"
-	
+	Write-Output "$chapterName Thesaurunocerous starts:"	
 	$chapter = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes 
 	$chapterTheasurus = $chapter | python thesaurunocerous.py | ConvertFrom-Json | %{ $_.Results }
 	$chapterTheasurus | fl
@@ -106,10 +105,41 @@ Function Thesaurunocerous-Chapter($chapterName, $wordsFilename) {
 			-Message "$($_.Word) x $($_.Occurs) - $chapterName" `
 			-Details "$($_.Hint)" `
 			-Category "$($_.Status)" 
-		}
+		}	
+	Write-Output "$chapterName Thesaurunocerous end!"	
 	
-	Write-Output "$chapterName Thesaurunocerous end!"
+	
+#
+# Word analysis chapter files by filename convention
+# Outputs word stats for files
+#
+Function WordAnalysis-Chapter($chapterName) {
+	# $chapterName = "Chapter One"
+	Write-Output "$chapterName WordAnalysis starts:"	
+	$chapterContent = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes 
+	$chapterWordCount = $chapterContent | python wordcounter.py | ConvertFrom-Csv 
+	# Four columns: Word, Length, Count, Percent: 
+	# - Word is the word
+	# - Length is it's length of characters 
+	# - Count is it's number of occurances in the chapter
+	# - Percent is it's percent occurance in the chapter
+	$chapterWordCount | `
+		Where { $_.Count -gt 1 } | `
+		Where { $_.Length -gt 2 }	
+	$chapterWordCount | Measure-Object Count -Sum -Maximum | Select -Property `
+		@{Label="Unique word count";Expression={$_.Count}}, 
+		@{label="Word count";Expression={$_.Sum}}, 
+		@{label="Maximum occurrence of any word";Expression={$_.Maximum}} | fl #`
+	$chapterWordHints = $chapterWordCount | `
+		Where { $_.Count -gt 1 } | `
+		Where { $_.Length -gt 2 } | `
+		foreach { $_.Word } | `
+		python theasaurus.py | `
+		ConvertFrom-Csv
+	Write-Output $chapterWordHints 
+	Write-Output "$chapterName WordAnalysis ends!"
 }
+
 
 # run tests
 Write-Output "Spelling Starts" 
@@ -124,31 +154,31 @@ Write-Output "Thesaurunocerous Starts"
 Thesaurunocerous-Chapter "Chapter One" "Chapter-One-Words.txt"
 Write-Output "Thesaurunocerous Ends"
 
+# word analysis
+WordAnalysis-Chapter "Chapter One" | Out-File "Chapter-One-Words.txt" -Append
 
 # Superfluous mucking about
-
 # Word counts
-$chapterName = "Chapter One"
-$chapterContent = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes 
-$chapterWordCount = $chapterContent | python wordcounter.py | ConvertFrom-Csv # Four columns: Word, Length, Count, Percent
-$chapterWordCount | `
-	Where { $_.Count -gt 1 } | `
-	Where { $_.Length -gt 2 }	
-$chapterWordCount | Measure-Object Count -Sum -Maximum | Select -Property `
-	@{Label="Unique word count";Expression={$_.Count}}, 
-	@{label="Word count";Expression={$_.Sum}}, 
-	@{label="Maximum occurrence of any word";Expression={$_.Maximum}} | fl #`
-
-
-$chapterWordHints = $chapterWordCount | `
-	Where { $_.Count -gt 1 } | `
-	Where { $_.Length -gt 2 } | `
-	foreach { $_.Word } | `
-	python theasaurus.py | `
-	ConvertFrom-Csv
-	
-Write-Output $chapterWordHints 
-
+#$chapterName = "Chapter One"
+#$chapterContent = Get-Content -Path "Prose - $chapterName*.md" -Encoding UTF8 | Replace-FancyQuotes 
+#$chapterWordCount = $chapterContent | python wordcounter.py | ConvertFrom-Csv # Four columns: Word, Length, Count, Percent
+#$chapterWordCount | `
+#	Where { $_.Count -gt 1 } | `
+#	Where { $_.Length -gt 2 }	
+#$chapterWordCount | Measure-Object Count -Sum -Maximum | Select -Property `
+#	@{Label="Unique word count";Expression={$_.Count}}, 
+#	@{label="Word count";Expression={$_.Sum}}, 
+#	@{label="Maximum occurrence of any word";Expression={$_.Maximum}} | fl #`
+#
+#
+#$chapterWordHints = $chapterWordCount | `
+#	Where { $_.Count -gt 1 } | `
+#	Where { $_.Length -gt 2 } | `
+#	foreach { $_.Word } | `
+#	python theasaurus.py | `
+#	ConvertFrom-Csv
+#	
+#Write-Output $chapterWordHints 
 # End of superfluous mucking about `
 
 
@@ -163,20 +193,26 @@ cat "Prose - Chapter One1.md",
 		"Prose - Chapter Two1.md", 
 		"Prose - Blank line.md" | sc "The-Return-of-Tom-Thumb.md" #`
 Get-Content "The-Return-of-Tom-Thumb.md" -Encoding UTF8 | Replace-FancyQuotes | Out-File "The-Return-of-Tom-Thumb.txt" -Encoding UTF8 -Append
-Write-Output "...The-Return-of-Tom-Thumb.md created"
+Write-Output "...The-Return-of-Tom-Thumb.md and The-Return-of-Tom-Thumb.txt created"
 
+Write-Output "Combining books ..."
 pandoc --version
 pandoc --css epubstyle.css `
   "title.md" `
   "The-Return-of-Tom-Thumb.md" `
   -o The-Return-of-Tom-Thumb.epub
+Write-Output "... made The-Return-of-Tom-Thumb.epub..."
 
 #html version
 pandoc --css epubstyle.css `
   "title.md" `
   "The-Return-of-Tom-Thumb.txt" `
   -o The-Return-of-Tom-Thumb.html
+Write-Output "... made The-Return-of-Tom-Thumb.html..."
 	
 # Make the audio book (WIP)
-Get-Content -Path "The-Return-of-Tom-Thumb.txt" -Encoding UTF8 | Destroy-FancyQuotes >test1.txt
+Get-Content -Path "The-Return-of-Tom-Thumb.txt" -Encoding UTF8 | Destroy-Quotes >test1.txt
 cat test1.txt | python .\googleTextToSpeech.py -o The-Return-of-Tom-Thumb.mp3 -d The-Return-of-Tom-Thumb.mp3.log
+Write-Output "... made The-Return-of-Tom-Thumb.mp3 and The-Return-of-Tom-Thumb.mp3.log..."
+Write-Output "Finished!"
+Write-Output "woos awesum YO'UR AWSUM"
