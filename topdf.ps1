1eacb6c4e302ac9bf28667a82f398a572e6b23ac
Matthew <PunkUnicorn100@hotmail.com>
2018-05-26 20:19:34 +0100

    spelling error fix

--- topdf.ps1
+++ topdf.ps1
@@ -43,7 +43,9 @@ Function Spellcheck-Chapter($chapterName, $spellingFailFilename) {
 	$chapterSpelling = $chapter | Replace-FancyQuotes | python spellchecker.py | ConvertFrom-Csv
 	$chapterSpelling 
 	
-	,$chapterSpelling  | Where {$_.Length -gt 0 } | ForEach { $_ } | Out-File -FilePath $spellingFailFilename -Append
+	,$chapterSpelling | Where {$_.Length -gt 0 } | ForEach { $_ } | `
+		Where {$_.Length -gt 0 } |`
+		Out-File -FilePath $spellingFailFilename -Append
 	
 	$chapterSpelling | `
 		%{ Add-AppveyorTest `
