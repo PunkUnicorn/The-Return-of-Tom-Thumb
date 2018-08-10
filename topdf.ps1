2329de6c83ccff769297c360eafe4cfa77d6539a
Matthew <PunkUnicorn100@hotmail.com>
2018-08-10 17:36:45 +0100

    appedix fix?

--- topdf.ps1
+++ topdf.ps1
@@ -198,6 +198,9 @@ soxi
 #
 Write-output `n | Out-File "Prose - Blank line.md" -Append
 Write-output "# Appendix One" | Out-File "Prose - Appendix1.md" -Append
+Write-output "# Appendix Two" | Out-File "Prose - Appendix2.md" -Append
+Write-output "# Appendix Three" | Out-File "Prose - Appendix3.md" -Append
+Write-output "# Appendix Four" | Out-File "Prose - Appendix4.md" -Append
 
 Write-Output "Adding build version to final-title.md..."
 Add-Content -Path "book-version.txt" -Value $env:APPVEYOR_BUILD_VERSION
@@ -222,8 +225,14 @@ cat "The-Return-of-Tom-Thumb-for-audio.md",
 	"Prose - Appendix1.md",
 	"Prose - Blank line.md",
 	"Character - Red Riding Hood\Red Riding Hood - D20 Model.md",
+	"Prose - Appendix2.md",
+	"Prose - Blank line.md",
 	"Character - Tom Thumb\Tom Thumb - D20 Model.md",
+	"Prose - Appendix3.md",
+	"Prose - Blank line.md",
 	"Character - The Knight\The Knight - D20 Model.md",
+	"Prose - Appendix4.md",
+	"Prose - Blank line.md",
 	"Character - The Tailor\The Tailor - D20 Model.md" | sc "The-Return-of-Tom-Thumb.md" 
 	
 Get-Content "The-Return-of-Tom-Thumb.md" -Encoding UTF8 | Replace-FancyQuotes | Out-File "The-Return-of-Tom-Thumb.txt" -Encoding UTF8 -Append
