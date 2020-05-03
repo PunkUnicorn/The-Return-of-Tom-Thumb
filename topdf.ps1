02dade27d67dbfd8f5877d8af680c57a4769bd4b
Matthew <matthewcocks@hotmail.com>
2020-05-03 16:43:48 +0100

    Filename Fix

--- topdf.ps1
+++ topdf.ps1
@@ -268,7 +268,7 @@ cat "book-version.txt",
 	"Prose - Chapter Four1.md",
 	"Prose - Blank line.md",
 	"Prose - Chapter Five1.md",
-	"Prose - Chapter Six.md"| sc "The-Return-of-Tom-Thumb-for-audio.md" 
+	"Prose - Chapter Six1.md"| sc "The-Return-of-Tom-Thumb-for-audio.md" 
 
 
 cat "The-Return-of-Tom-Thumb-for-audio-single-chapter-one.md" | sc "The-Return-of-Tom-Thumb-single-chapter-one.md" 
