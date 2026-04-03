dc488c323b5ba58862e9217430e7443367f96347
Matthew <matthewcocks@hotmail.com>
2026-04-03 17:50:03 +0100

    Oops I did need Scriban. Replaced Scriban with Replace

--- PremiumAudioBookSdk/TomThumbPremiumAudioBook/TomThumbPremiumAudioBook/Program.cs
+++ PremiumAudioBookSdk/TomThumbPremiumAudioBook/TomThumbPremiumAudioBook/Program.cs
@@ -139,7 +139,7 @@ namespace TomThumbPremiumAudioBook
                 using (var synthesizer = new SpeechSynthesizer(config, audioConfig))
                 {
                     //Console.WriteLine("doing ");
-                    var ssml = File.ReadAllText(inputSsml);
+                    var ssml = File.ReadAllText(inputSsml).Replace("{{content}}", model["content"]);
                     //var template = Scriban.Template.Parse(ssml);
                     var content = ssml; //template.Render(model);
 
